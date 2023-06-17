99b4db79bcdcb2e3e19cf420f1803665113133f0
Abdallah Mohamed <abdal_cascad@hotmail.com>
2023-06-17 19:46:30 -0400

    first-commit

--- src/JSConnectorService.php
+++ src/JSConnectorService.php
@@ -2,17 +2,12 @@
 
 namespace Amohamed\JSConnector;
 
-use Exception;
 use GuzzleHttp\Client;
-use GuzzleHttp\Middleware;
 use GuzzleHttp\HandlerStack;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Http;
-use Symfony\Component\Process\Process;
-use Illuminate\Support\Facades\Artisan;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Exception\RequestException;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-
+use Illuminate\Support\Facades\Log;
 
 class JSConnectorService
 {
@@ -27,9 +22,8 @@ class JSConnectorService
             // Retry on connection timeouts or server errors
             return $exception instanceof RequestException || ($response && $response->getStatusCode() >= 500);
         }));
-        // C:\laragon\www\langchain-laravel\config\jsconnector.php
+
         $this->baseUrl = config('jsconnector.api_url', 'http://localhost:3000');
-        Log::info('baseUrl: ' . $this->baseUrl);
 
         $this->client = new Client([
             'base_uri' => $this->baseUrl,
@@ -40,17 +34,17 @@ class JSConnectorService
 
     public function post($endpoint, $data)
     {
-        return $this->request('POST', $endpoint, $data);
+        return $this->request('POST', $endpoint, ['json' => $data]);
     }
 
     public function get($endpoint, $query = [])
     {
-        return $this->request('GET', $endpoint, [], $query);
+        return $this->request('GET', $endpoint, ['query' => $query]);
     }
 
     public function put($endpoint, $data)
     {
-        return $this->request('PUT', $endpoint, $data);
+        return $this->request('PUT', $endpoint, ['json' => $data]);
     }
 
     public function delete($endpoint)
@@ -58,80 +52,17 @@ class JSConnectorService
         return $this->request('DELETE', $endpoint);
     }
 
-    protected function request($method, $endpoint, $data = [], $query = [])
+    protected function request($method, $endpoint, $options = [])
     {
-        // $this->startServerIfNotRunning();
-
         $url = $this->baseUrl . '/' . ltrim($endpoint, '/');
         Log::info('url: ' . $url);
-        Log::info('data: ' . json_encode($data));
-        if ($method === 'GET' && !empty($query)) {
-            $url .= '?' . http_build_query($query);
-        }
 
         try {
-            $response = Http::withHeaders([
-                'Content-Type' => 'application/json',
-            ])->$method($url, $data);
-
-            return $response->json();
+            $response = $this->client->request($method, $url, $options);
+            return json_decode($response->getBody(), true);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             return response()->json(['error' => 'Server error. Please try again later.'], 500);
         }
     }
-
-    // //check if the command jsconnector is not running if not then run the command
-    // protected function startServerIfNotRunning()
-    // {
-    //     $url = $this->baseUrl . '/healthcheck';
-
-    //     try {
-    //         $response = Http::get($url);
-
-    //         if ($response->successful()) {
-    //             Log::info('Server is running');
-    //             return true;
-    //         }
-    //     } catch (\Exception $e) {
-    //         if ($e instanceof \GuzzleHttp\Exception\ConnectException) {
-    //             // If the server is down, start it
-    //             $command = ['node', env('JSCONNECTOR_SERVER_PATH', 'resources/js/langchain.cjs')];
-    //             $process = Process::fromShellCommandline(implode(' ', $command));
-
-    //             try {
-    //                 // Run the process in the background
-    //                 $process->start();
-
-    //                 // Immediately return, letting the request processing continue
-    //                 return true;
-    //             } catch (\Exception $exception) {
-    //                 Log::error('Process failed. Error: ' . $exception->getMessage());
-
-    //                 // Show error to user
-    //                 return response()->json(['error' => 'Server could not be started.'], 500);
-    //             }
-    //         }
-    //     }
-
-    //     return false;
-    // }
-
-    // public function isServerRunning()
-    // {
-    //     $url = $this->baseUrl . '/healthcheck';
-
-    //     try {
-    //         $response = Http::get($url);
-
-    //         if ($response->successful()) {
-    //             return true;
-    //         }
-    //     } catch (\Exception $e) {
-    //         // Log the error
-    //         Log::error('Error checking server health: ' . $e->getMessage());
-    //     }
-
-    //     return false;
-    // }
 }
